0a6bacaa599b00b3eccb255e031eec428712a510
SAUVE Jean-Luc <sauvejeanluc3@gmail.com>
2021-03-06 13:34:25 +0200

    Wrote CRUD for OrderController and OrderedItemController

--- src/main/java/com/openrca/ecommerce/controller/OrderController.java
+++ src/main/java/com/openrca/ecommerce/controller/OrderController.java
@@ -1,4 +1,61 @@
 package com.openrca.ecommerce.controller;
 
+import com.openrca.ecommerce.model.Order;
+import com.openrca.ecommerce.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
+
+import java.util.Optional;
+
+@RestController
 public class OrderController {
+
+    @Autowired
+    private OrderRepository orderRepository;
+
+    @PostMapping("/order")
+    public Order addOrderDetails(@RequestBody Order order){
+        return orderRepository.save(order);
+    }
+
+    @GetMapping("/order/{id}")
+    public Order getOrderById(@PathVariable Long id){
+        return orderRepository.findById(id).
+                orElseThrow(()-> new HttpClientErrorException(HttpStatus.NOT_FOUND));
+    }
+
+
+    @PutMapping("/order/{id}")
+    public ResponseEntity<Order> updateOrderById(@PathVariable Long id, @RequestBody Order order){
+
+        Optional<Order> orderData = orderRepository.findById(id);
+
+        if( orderData.isPresent() ){
+            Order _order = orderData.get();
+            _order.setOrderId(order.getOrderId());
+            _order.setCustomerId(order.getCustomerId());
+            _order.setShipId(order.getShipId());
+            _order.setTotalNumberOfOrderedProduct(order.getTotalNumberOfOrderedProduct());
+            _order.setTotalOrderedCost(order.getTotalOrderedCost());
+            _order.setTotalDiscount(order.getTotalDiscount());
+            _order.setOrderedAt(order.getOrderedAt());
+            _order.setOrderStatus(order.getOrderStatus());
+            _order.setShippingMethod(order.getShippingMethod());
+
+            return new ResponseEntity<>(orderRepository.save(_order), HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/order/{id}")
+    public void deleteCustomerById(@PathVariable Long id){
+        orderRepository.deleteById(id);
+    }
+
+
 }
--- src/main/java/com/openrca/ecommerce/controller/OrderedItemController.java
+++ src/main/java/com/openrca/ecommerce/controller/OrderedItemController.java
@@ -1,4 +1,59 @@
 package com.openrca.ecommerce.controller;
 
+import com.openrca.ecommerce.model.OrderedItem;
+import com.openrca.ecommerce.repository.OrderedItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.HttpClientErrorException;
+
+import java.util.Optional;
+
+@RestController
 public class OrderedItemController {
+
+    @Autowired
+    private OrderedItemRepository orderedItemRepository;
+
+    @PostMapping("/orderedItem")
+    public OrderedItem addOrderDetails(@RequestBody OrderedItem orderedItem){
+        return orderedItemRepository.save(orderedItem);
+    }
+
+    @GetMapping("/orderedItem/{id}")
+    public OrderedItem getOrderedItemById(@PathVariable Long id){
+        return orderedItemRepository.findById(id).
+                orElseThrow(()-> new HttpClientErrorException(HttpStatus.NOT_FOUND));
+    }
+
+
+    @PutMapping("/orderedItem/{id}")
+    public ResponseEntity<OrderedItem> updateOrderById(@PathVariable Long id, @RequestBody OrderedItem orderedItem){
+
+        Optional<OrderedItem> orderedItemData = orderedItemRepository.findById(id);
+
+        if( orderedItemData.isPresent() ){
+            OrderedItem _order = orderedItemData.get();
+            _order.setId(orderedItem.getId());
+            _order.setProductId(orderedItem.getProductId());
+            _order.setOrderedId(orderedItem.getOrderedId());
+            _order.setProductCategoryId(orderedItem.getProductCategoryId());
+            _order.setUnit_price(orderedItem.getUnit_price());
+            _order.setAmount(orderedItem.getAmount());
+            _order.setCreatedAt(orderedItem.getCreatedAt());
+
+            return new ResponseEntity<>(orderedItemRepository.save(_order), HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/orderedItem/{id}")
+    public void deleteCustomerById(@PathVariable Long id){
+        orderedItemRepository.deleteById(id);
+    }
+
+
 }
